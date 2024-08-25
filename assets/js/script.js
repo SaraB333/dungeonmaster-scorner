5e834bd726b83433e271ce05d11da4c0882eac28
Sara Blackburn <Sara.blackburn31@gmail.com>
2024-08-25 18:11:41 +0000

    added an array of questions and answers to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,60 @@
 // Wait for the DOM to load fully before running any Javascript
 document.addEventListener("DOMContentLoaded", () => {
 
+    // An array of questions and answers that will populate the monster facts game
+    let monsterQuestions = [
+
+        {
+            question: "Which type of Beholder has suppressed its xenophobia and paranoia, and lives as a leader of a community?",
+            answers: ["Death Tyrants", "Eye Tyrants", "Death Kiss", "Beholder Hives"],
+            correct: 1
+        },
+        {
+            question: "Which type of Hag will MOST COMMONLY be found in wintery lands, favoring snow covered mountain peaks?",
+            answers: ["Annis Hags", "Green Hags", "Bheur Hags", "Night Hags"],
+            correct: 2
+        },
+        {
+            question: "Fill in the blank. A Kobold Dragonshield is '..' by a Dragon to protect it's eggs.",
+            answers: ["Killed", "Chosen", "Born", "Lives"],
+            correct: 1
+        },
+        {
+            question: "Which of the following types of Mind Flayer was originally a Beholder?",
+            answers: ["Mindwitness", "Ulitharid", "Alhoon", "Elder Brain"],
+            correct: 0
+        },
+        {
+            question: "Which type of Giant used to be the officers, engineers and crafters of ancient Ostoria?",
+            answers: ["Cloud Giants", "Frost Giants", "Stone Giants", "Fire Giants"],
+            correct: 3
+        },
+        {
+            question: "Which animal do Gnolls transform from?",
+            answers: ["Lions", "Wolves", "Hyenas", "Wild Dogs"],
+            correct: 2
+        },
+        {
+            question: "Fill in the blank. Khurcorbaeyac's holy symbol is a yellow-and-red striped '..' made of leather.",
+            answers: ["Armor set", "Club", "Boomerang", "Whip"],
+            correct: 3
+        },
+        {
+            question: "Which goblin served under a hobgoblin wizard, stole a look at its master's spellbook, and learned a little wizardry?",
+            answers: ["Booyahg Slave", "Booyahg Caster", "Booyahg Wielder", "Booyahg Whip"],
+            correct: 1
+        },
+        {
+            question: "How do kenku communicate, given their inability to speak in their own voices?",
+            answers: ["Mimicry", "Telepathy", "Writing", "Sign Language"],
+            correct: 0
+        },
+        {
+            question: "What is the only thing a Devourer needs to survive?",
+            answers: ["Air", "Sleep", "Blood", "Souls"],
+            correct: 3
+        }
+    ]
+
 
 })
