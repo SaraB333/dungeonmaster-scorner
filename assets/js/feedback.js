f6f9ccd87ab106d370b73d5904772997290cfe4d
Sara Blackburn <Sara.blackburn31@gmail.com>
2024-08-26 14:24:59 +0000

    feedback page made for sign up form completion, styling added and home button

---
+++ assets/js/feedback.js
@@ -0,0 +1,12 @@
+// Wait for the DOM to load fully before running any Javascript
+document.addEventListener("DOMContentLoaded", () => {
+
+    //function to bring the user back to the home page
+    function goHome() {
+        window.location.href = 'index.html';
+    }
+
+    // brings the function into the global window for the onclick features
+    window.goHome = goHome;
+
+})
